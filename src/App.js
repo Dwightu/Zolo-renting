cba020cc3ee6d44b48378804620eb61a06425a85
Dwightu <yudehui1115@gmail.com>
2022-05-08 17:51:49 -0400

    Save User To Firestore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import SingIn from './pages/SignIn'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import ForgotPassword from './pages/ForgotPassword'
 
 
 function App() {
   return (
     <>
-      <h1>My app</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SingIn />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
     </>
   );
 }
